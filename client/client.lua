44f933cf51d69f73f4c1f6e7e5bde586c3c541b5
RexShack <rexshackuk@gmail.com>
2022-10-30 12:23:07 +0000

    Initial commit

---
+++ client/client.lua
@@ -0,0 +1,51 @@
+local QRCore = exports['qr-core']:GetCoreObject()
+local miningstarted = false
+local mining
+
+--------------------------------------------------------------------------
+
+-- mining locations
+Citizen.CreateThread(function()
+    for mining, v in pairs(Config.MiningLocations) do
+        exports['qr-core']:createPrompt(v.location, v.coords, QRCore.Shared.Keybinds['J'], 'Start ' .. v.name, {
+            type = 'client',
+            event = 'rsg-mining:client:StartMining',
+        })
+        if v.showblip == true then
+			local MiningBlip = Citizen.InvokeNative(0x554D9D53F696D002, 1664425300, v.coords)
+            SetBlipSprite(MiningBlip, GetHashKey("blip_ambient_tracking"), 1)
+            SetBlipScale(MiningBlip, 0.2)
+			Citizen.InvokeNative(0x9CB1A1623062F402, MiningBlip, v.name)
+        end
+    end
+end)
+
+--------------------------------------------------------------------------
+
+-- start mining
+RegisterNetEvent('rsg-mining:client:StartMining')
+AddEventHandler('rsg-mining:client:StartMining', function()
+	local player = PlayerPedId()
+	local hasItem = QRCore.Functions.HasItem('pickaxe', 1)
+	if miningstarted == false then
+		if hasItem then
+			local randomNumber = math.random(1,100)
+			if randomNumber > 90 then -- 10% chance of pickace breaking
+				TriggerServerEvent('rsg-mining:server:removeitem', 'pickaxe')
+			else
+				miningstarted = true
+				TaskStartScenarioInPlace(player, GetHashKey('WORLD_HUMAN_PICKAXE_WALL'), -1, true, false, false, false)
+				Wait(18000)
+				ClearPedTasksImmediately(player)
+				SetCurrentPedWeapon(player, `WEAPON_UNARMED`, true)
+				RemoveAllPedWeapons(player, true, true)
+				TriggerServerEvent('rsg-mining:server:giveMiningReward')
+				miningstarted = false
+			end
+		else
+			QRCore.Functions.Notify('you don\'t have a pickaxe!', 'error')
+		end
+	else
+		QRCore.Functions.Notify('you are busy at the moment!', 'primary')
+	end
+end)
---
+++ server/server.lua
@@ -0,0 +1,61 @@
+local QRCore = exports['qr-core']:GetCoreObject()
+
+-- give mining reward
+RegisterServerEvent('rsg-mining:server:giveMiningReward')
+AddEventHandler('rsg-mining:server:giveMiningReward', function()
+    local src = source
+    local Player = QRCore.Functions.GetPlayer(src)
+	local chance = math.random(1,100)
+	if chance <= 50 then
+		local item1 = Config.MiningRewards[math.random(1, #Config.MiningRewards)]
+		local item2 = Config.MiningRewards[math.random(1, #Config.MiningRewards)]
+		local item3 = Config.MiningRewards[math.random(1, #Config.MiningRewards)]
+		-- add items
+        Player.Functions.AddItem(item1, Config.SmallRewardAmount)
+        TriggerClientEvent("inventory:client:ItemBox", src, QRCore.Shared.Items[item1], "add")
+		Player.Functions.AddItem(item2, Config.SmallRewardAmount)
+        TriggerClientEvent("inventory:client:ItemBox", src, QRCore.Shared.Items[item2], "add")
+		Player.Functions.AddItem(item3, Config.SmallRewardAmount)
+        TriggerClientEvent("inventory:client:ItemBox", src, QRCore.Shared.Items[item3], "add")
+		TriggerClientEvent('QRCore:Notify', src, 'small haul time!', 'primary')
+	elseif chance >= 50 and chance <= 80 then -- medium reward
+		local item1 = Config.MiningRewards[math.random(1, #Config.MiningRewards)]
+		local item2 = Config.MiningRewards[math.random(1, #Config.MiningRewards)]
+		local item3 = Config.MiningRewards[math.random(1, #Config.MiningRewards)]
+		-- add items
+        Player.Functions.AddItem(item1, Config.MediumRewardAmount)
+        TriggerClientEvent("inventory:client:ItemBox", src, QRCore.Shared.Items[item1], "add")
+		Player.Functions.AddItem(item2, Config.MediumRewardAmount)
+        TriggerClientEvent("inventory:client:ItemBox", src, QRCore.Shared.Items[item2], "add")
+		Player.Functions.AddItem(item3, Config.MediumRewardAmount)
+        TriggerClientEvent("inventory:client:ItemBox", src, QRCore.Shared.Items[item3], "add")
+		TriggerClientEvent('QRCore:Notify', src, 'medium haul this time!', 'primary')
+	elseif chance > 80 then -- large reward
+		local item1 = Config.MiningRewards[math.random(1, #Config.MiningRewards)]
+		local item2 = Config.MiningRewards[math.random(1, #Config.MiningRewards)]
+		local item3 = Config.MiningRewards[math.random(1, #Config.MiningRewards)]
+		-- add items
+        Player.Functions.AddItem(item1, Config.LargeRewardAmount)
+        TriggerClientEvent("inventory:client:ItemBox", src, QRCore.Shared.Items[item1], "add")
+		Player.Functions.AddItem(item2, Config.LargeRewardAmount)
+        TriggerClientEvent("inventory:client:ItemBox", src, QRCore.Shared.Items[item2], "add")
+		Player.Functions.AddItem(item3, Config.LargeRewardAmount)
+        TriggerClientEvent("inventory:client:ItemBox", src, QRCore.Shared.Items[item3], "add")
+		TriggerClientEvent('QRCore:Notify', src, 'large haul this time!', 'primary')
+	end
+end)
+
+-- remove pickaxe if broken
+RegisterServerEvent('rsg-mining:server:removeitem')
+AddEventHandler('rsg-mining:server:removeitem', function(item)
+	local src = source
+    local Player = QRCore.Functions.GetPlayer(src)
+	if item == 'pickaxe' then
+		Player.Functions.RemoveItem('pickaxe', 1)
+		TriggerClientEvent("inventory:client:ItemBox", src, QRCore.Shared.Items['pickaxe'], "add")
+		QRCore.Functions.Notify(src, 'your pickaxe broke!', 'success')
+	else
+		QRCore.Functions.Notify(src, 'something went wrong!', 'error')
+		print('something went wrong with moonshine script could be exploint!')
+	end
+end)
