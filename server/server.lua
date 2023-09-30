f884367195e6eb80023c50536c89c9025b3a0a2e
RexshackGaming <rexshackuk@gmail.com>
2023-09-30 14:21:20 +0100

    version checker

--- server/server.lua
+++ server/server.lua
@@ -1,5 +1,36 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
+-----------------------------------------------------------------------
+-- version checker
+-----------------------------------------------------------------------
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-mining/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then 
+            versionCheckPrint('error', 'Currently unable to run a version check.')
+            return 
+        end
+
+        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
+        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
+        
+        if text == currentVersion then
+            versionCheckPrint('success', 'You are running the latest version.')
+        else
+            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
+        end
+    end)
+end
+
+-----------------------------------------------------------------------
+
 -- give mining reward
 RegisterServerEvent('rsg-mining:server:giveMiningReward')
 AddEventHandler('rsg-mining:server:giveMiningReward', function()
@@ -56,6 +87,11 @@ AddEventHandler('rsg-mining:server:removeitem', function(item)
         RSGCore.Functions.Notify(src, Lang:t('success.your_pickaxe_broke'), 'success')
     else
         RSGCore.Functions.Notify(src, Lang:t('error.something_went_wrong'), 'error')
-        print('something went wrong with moonshine script could be exploint!')
+        print('something went wrong with the script could be exploint!')
     end
 end)
+
+--------------------------------------------------------------------------------------------------
+-- start version check
+--------------------------------------------------------------------------------------------------
+CheckVersion()
